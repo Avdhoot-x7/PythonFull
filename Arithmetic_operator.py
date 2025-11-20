ac605db8d78f2445653812a189604504d8a3a5f2
Avadhut Makarand Chakradeo <avdhootchakradeo@gmail.com>
2025-11-20 17:09:00 +0530

    Add files via upload

---
+++ Arithmetic_operator.py
@@ -0,0 +1,29 @@
+# Arithmetic Operators in Python
+
+# Addition
+add_result = 10 + 5
+print("Addition (10 + 5):", add_result)
+
+# Subtraction
+sub_result = 10 - 3
+print("Subtraction (10 - 3):", sub_result)
+
+# Multiplication
+mul_result = 4 * 5
+print("Multiplication (4 * 5):", mul_result)
+
+# Division
+div_result = 10 / 4
+print("Division (10 / 4):", div_result)
+
+# Floor Division
+floor_result = 10 // 4
+print("Floor Division (10 // 4):", floor_result)
+
+# Modulus
+mod_result = 10 % 4
+print("Modulus (10 % 4):", mod_result)
+
+# Exponential
+exp_result = 2 ** 3
+print("Exponential (2 ** 3):", exp_result)
